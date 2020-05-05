af7876d68544a7fcb4ff2f50c1e0b9394add4448
Brady Borman <bradenborman@hotmail.com>
2020-05-04 20:40:30 -0500

    Accepts single object as list now

--- mfl-server/src/main/java/borman/myfantasyleague/config/AppConfig.java
+++ mfl-server/src/main/java/borman/myfantasyleague/config/AppConfig.java
@@ -1,7 +1,5 @@
 package borman.myfantasyleague.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -12,12 +10,6 @@ public class AppConfig {
     @Bean
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
-    }
-
-    @Bean
-    public ObjectMapper jacksonObjectMapper() {
-        return new ObjectMapper().setPropertyNamingStrategy(
-                PropertyNamingStrategy.SNAKE_CASE);
     }
 
 }
--- mfl-server/src/main/java/borman/myfantasyleague/models/draftpickdata/CurrentYearDraftPicks.java
+++ mfl-server/src/main/java/borman/myfantasyleague/models/draftpickdata/CurrentYearDraftPicks.java
@@ -1,5 +1,6 @@
 package borman.myfantasyleague.models.draftpickdata;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
@@ -8,6 +9,7 @@ import java.util.List;
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class CurrentYearDraftPicks {
 
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
     private List<DraftPick> draftPick;
 
     public List<DraftPick> getDraftPick() {
